b197a5a30e041128d9e7cd2f2aacc164872f6afa
nagatai <nagayama90@gmail.com>
2019-01-10 16:26:36 +0900

    ResultModalのスクリプトとプレハブ追加

---
+++ Assets/Scripts/ResultModalModel.cs
@@ -0,0 +1,13 @@
+﻿
+public class ResultModalModel {
+    
+    public readonly float averageTime;
+    private readonly Context context;
+
+    public ResultModalModel(float averageTime, Context context){
+        this.averageTime = averageTime;
+        this.context = context;
+    }
+
+
+}
---
+++ Assets/Scripts/ResultModalPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResultModalPresenter : UtilComponent
+{
+    
+    [SerializeField] private Text averageTime;
+
+    public void Show(ResultModalModel model) {
+        SetLabel(this.averageTime, model.averageTime.ToString("F2"));
+        //model.Start.Init("Start", model.Context);
+        this.gameObject.SetActive(true);
+    }
+
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Reset()
+    {
+        // Gamestrap.GSAppExampleControl.Instance.LoadScene(Gamestrap.ESceneNames.scene_title);
+    }
+
+}
