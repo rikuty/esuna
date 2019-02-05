2d2d0fb5e07f6e0d09f4ae67996656893d758132
LunaSun <kawano@luna-sun.com>
2019-02-05 14:22:31 +0900

    タッチパネル雛形

---
+++ Assets/Scripts/PanelButtonComponent.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PanelButtonComponent : UtilComponent
+{
+
+
+    [SerializeField] MeshRenderer cubeRenderer;
+
+    [SerializeField] GameObject objParticle;
+
+    [SerializeField] GameObject objCube;
+
+    [SerializeField] AudioSource audioSource;
+
+    Action<PanelButtonComponent> callbackAnswer;
+
+
+    [NonSerialized] public DEFINE_APP.ANSWER_TYPE_ENUM answerType;
+    [NonSerialized] public int answerIndex;
+    [NonSerialized] public Nest touchNest;
+
+
+    float TIME_STAY = 1f;
+
+    bool enter = false;
+
+    float deltaTime = 0f;
+
+    //Rigidbody rigidbody;
+    //bool collisionFromUpper = false;
+    //bool collisionFromLower = false;
+
+    public void Init(Action<PanelButtonComponent> callbackAnswer, DEFINE_APP.ANSWER_TYPE_ENUM cubeType, int answerIndex)
+    {
+        this.callbackAnswer = callbackAnswer;
+        this.answerType = cubeType;
+        this.answerIndex = answerIndex;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!enter) return;
+        deltaTime += Time.deltaTime;
+        if (deltaTime > TIME_STAY)
+        {
+            enter = false;
+            //collisionFromUpper = false;
+            deltaTime = 0f;
+
+            objParticle.SetActive(true);
+            objCube.SetActive(false);
+            if (touchNest != null)
+            {
+                touchNest.SetActiveNest(false);
+                audioSource.Play();
+
+            }
+            StartCoroutine("Coroutine");
+
+        }
+
+    }
+
+
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        gameObject.SetActive(false);
+        callbackAnswer(this);
+        Destroy(this.gameObject);
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("OnCollisionEnter" + collision.gameObject.name);
+        childColliderComponent childColliderComponent = collision.collider.GetComponent<childColliderComponent>();
+
+        if (childColliderComponent != null && childColliderComponent.nest != null)
+        {
+            //if (nest.answerType == answerType
+            //    && nest.answerIndex == answerIndex
+            //    /*&& collisionFromUpper*/)
+            //{
+            touchNest = childColliderComponent.nest;
+            enter = true;
+            //}
+        }
+
+        Terrain terrain = collision.collider.GetComponent<Terrain>();
+        if (terrain != null)
+        {
+            enter = true;
+            deltaTime = 0f;
+        }
+
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        enter = false;
+    }
+
+    //一旦上から入る判定は入れない
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    Nest nest = collision.collider.GetComponent<Nest>();
+    //    if (nest == null) return;
+    //}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("OnTriggerEnter" + other.gameObject.name + collisionFromUpper.ToString() + collisionFromLower.ToString());
+
+    //    if (other.gameObject.name == "nest" && !collisionFromLower)
+    //    {
+    //        collisionFromUpper = true;
+
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("OnCollisionExit" + other.gameObject.name + collisionFromUpper.ToString() + collisionFromLower.ToString());
+
+    //    if (other.gameObject.name == "nest")
+    //    {
+    //        collisionFromUpper = false;
+    //        collisionFromLower = false;
+
+
+    //    }
+    //}
+}
