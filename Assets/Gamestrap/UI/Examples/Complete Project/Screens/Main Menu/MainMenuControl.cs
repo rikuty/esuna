ca0cf1efd01ab2d0faf2d22b4b1230fc35042f2a
rikuty <info@rikuty.com>
2019-02-07 01:05:22 +0900

    タイトルシーンでオブジェクトを触った際にメインシーンに遷移

--- Assets/Gamestrap/UI/Examples/Complete Project/Screens/Main Menu/MainMenuControl.cs
+++ Assets/Gamestrap/UI/Examples/Complete Project/Screens/Main Menu/MainMenuControl.cs
@@ -74,9 +74,9 @@ namespace Gamestrap
 
         private void Update()
         {
-            bool result = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger);
-            if (result)
-                PlayClick();
+            //bool result = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger);
+            //if (result)
+            //    PlayClick();
         }
 
         public void AchievementsClick()
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using Gamestrap;
+
+
+public class MainMenuController : UtilComponent
+{
+
+    private string userID;
+    private string userName;
+    private DateTime dateTime;
+    public Text txtID;
+    public Text txtName;
+
+    public HandController handController;
+
+    private GameData gameData;
+
+    public void Start()
+    {
+
+        this.gameData = GameData.Instance;
+
+        this.dateTime = DateTime.Now;
+        this.userID = this.dateTime.ToString("yyMMddHHmm");
+        SetLabel(this.txtID, this.userID);
+        SetLabel(this.txtName, this.userName);
+
+        handController.Init(LoadMain);
+
+    }
+
+    public void LoadMain()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+}
