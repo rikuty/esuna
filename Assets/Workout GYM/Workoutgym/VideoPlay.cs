94b52d3598deef42b89d148a497d08d3242c74d7
rikuty <info@rikuty.com>
2019-02-07 01:25:09 +0900

    Titleをジムに

---
+++ Assets/Workout GYM/Workoutgym/VideoPlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// this line of code will make the Movie Texture begin playing
+		//((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
