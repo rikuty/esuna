bd712078867aefbf386fc122418dab3c1333e47f
rikuty <info@rikuty.com>
2019-02-12 09:27:52 +0900

    panelButtonComponentの中で振動するように設定

--- Assets/Scripts/PanelButtonComponent.cs
+++ Assets/Scripts/PanelButtonComponent.cs
@@ -14,27 +14,42 @@ public class PanelButtonComponent : UtilComponent
     [SerializeField] Material changeMaterial2;
 
     Action callback;
-
+    
     bool isTouch = false;
     float posZbase = 0;
     float posZcheck = 0;
-    
+
+    OVRGrabberBothHands grabbeHandsRight;
+    OVRGrabberBothHands grabberHandsLeft;
+
+
+    private void Start()
+    {
+        if(objRightHand != null && objLeftHand != null)
+        {
+            grabbeHandsRight = objRightHand.GetComponent<OVRGrabberBothHands>();
+            grabberHandsLeft = objLeftHand.GetComponent<OVRGrabberBothHands>();
+        }
+    }
+
     public void Init(Action callback)
     {
         this.callback = callback;
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
         if (isTouch)
         {
+
             posZcheck = objRightHand.transform.position.z - posZbase;
             if (posZcheck > 0.1f)
             {
                 //cubeRenderer.material = changeMaterial2;
                 animator.SetTrigger("PushTrigger");
+                isTouch = false;
             }
         }
     }
@@ -46,6 +61,8 @@ public class PanelButtonComponent : UtilComponent
 
         //とりあえず右手で判定
         posZbase = objRightHand.transform.position.z;
+        grabbeHandsRight.HapticsHands();
+
     }
 
     void OnCollisionExit(Collision collision)
