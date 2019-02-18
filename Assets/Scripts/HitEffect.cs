3aaf14ff24b0ef22d6a2edc36bd8f94a747c319e
LunaSun <kawano@luna-sun.com>
2019-02-18 16:09:14 +0900

    ヒットエフェクト追加

---
+++ Assets/Scripts/HitEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour {
+
+    [SerializeField] private Animator hitAnimator;
+    [SerializeField] private Animator birdAnimator;
+
+
+    // Use this for initialization
+    void Start () {
+        hitAnimator.SetTrigger("HitTrigger");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void SetFlyingStatus(int isFly) {
+        bool isFlying = (isFly == 1) ? true : false;
+        birdAnimator.SetBool("flying", isFlying);
+    }
+
+    void HitCallback()
+    {
+        Debug.Log("callback.");
+    }
+}
