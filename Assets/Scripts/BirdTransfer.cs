a814bc9d15b22fad00ddab002e5dcf4e6aab8158
LunaSun <kawano@luna-sun.com>
2019-02-19 14:35:09 +0900

    鳥モーションサンプル

---
+++ Assets/Scripts/BirdTransfer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdTransfer : MonoBehaviour
+{
+
+    [SerializeField] private Animator transferAnimator;
+    [SerializeField] private Animator birdAnimator;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayStart()
+    {
+        transferAnimator.SetTrigger("StartTrigger");
+    }
+
+    void PlayFlying()
+    {
+        transferAnimator.SetTrigger("FlyingTrigger");
+    }
+
+    void SetHitTrigger()
+    {
+        birdAnimator.SetTrigger("Hit");
+    }
+
+    void SetSitDownTrigger()
+    {
+        birdAnimator.SetTrigger("SitDown");
+    }
+
+    void SetSoarTrigger()
+    {
+        birdAnimator.SetTrigger("Soar");
+    }
+
+    void SetLandingStatus(int isLanding)
+    {
+        bool isLand = (isLanding == 1) ? true : false;
+        birdAnimator.SetBool("Landing", isLand);
+    }
+
+    void SetForward(float value)
+    {
+        birdAnimator.SetFloat("Forward", value);
+    }
+
+    void SetTurn(float value)
+    {
+        birdAnimator.SetFloat("Turn", value);
+    }
+
+    void StartCallback()
+    {
+        Debug.Log("start callback.");
+    }
+
+    void FlyingCallback()
+    {
+        Debug.Log("flying callback.");
+    }
+}
