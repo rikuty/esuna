4cfda395705ea5c458cd9785079aa01ce35a4252
永山 大志 <nagatai0120@gmail.com>
2019-02-27 14:16:47 +0900

    Gymアセットを再インポート

--- Assets/Workout GYM/Workoutgym/VideoPlay.cs
+++ Assets/Workout GYM/Workoutgym/VideoPlay.cs
@@ -6,7 +6,7 @@ public class VideoPlay : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		// this line of code will make the Movie Texture begin playing
-		//((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
 	}
 	
 	// Update is called once per frame
