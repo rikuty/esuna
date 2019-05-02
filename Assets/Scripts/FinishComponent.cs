7af9539070e61d8a70f62914b22d1c29857fb948
LunaSun <kawano@luna-sun.com>
2019-05-02 14:47:54 +0900

    Finishサンプル追加

---
+++ Assets/Scripts/FinishComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FinishComponent : MonoBehaviour {
+
+    [SerializeField] private Animator animator;
+
+    Action callback;
+
+	// Use this for initialization
+    public void Init(Action callback )
+    {
+        this.callback = callback;
+
+    }
+
+    private void FinishCallback(){
+        this.callback();
+    }
+}
