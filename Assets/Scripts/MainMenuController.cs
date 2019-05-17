cb9676b131d9f4999b952da4a7ff2109c41f768b
LunaSun <kawano@luna-sun.com>
2019-05-17 18:05:20 +0900

    MainMenuControllerにダミーデータでの通信追加

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using Gamestrap;
 
@@ -104,6 +105,7 @@ public class MainMenuController : UtilComponent
     private void FinishPushButton()
     {
         //measureController.StartDiagnosis();
+        ApiSetUserData();
         Camera.main.GetComponent<SceenFade>().LoadSceenWithFade("Game");
 
     }
@@ -112,11 +114,34 @@ public class MainMenuController : UtilComponent
     {
         //UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
         SetActive(objWarpEffect, true);
+        ApiSetUserData();
         Camera.main.GetComponent<SceenFade>().LoadSceenWithFade("Game");
     }
 
     private void FinishDiagnosis()
     {
         currentStatus = MENU_STATUS_ENUM.WAIT;
+    }
+
+    // TODO 実データで繋ぎなおし
+    private void ApiSetUserData(){
+        // サーバへPOSTするデータを設定 
+        string url = "http://dev.rikuty.net/api/SetUserData.php";
+
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        dic.Add("user_id", "1");
+        dic.Add("max_rom_measure_1", "44");
+        dic.Add("max_rom_measure_2", "44");
+        dic.Add("max_rom_measure_3", "44");
+        dic.Add("max_rom_measure_4", "44");
+        dic.Add("max_rom_measure_5", "44");
+        dic.Add("max_rom_measure_6", "44");
+        dic.Add("max_rom_measure_7", "44");
+        dic.Add("max_rom_measure_8", "44");
+        dic.Add("pre_rest_pain", "5");
+        dic.Add("pre_move_pain", "5");
+        dic.Add("pre_move_fear", "5");
+
+        StartCoroutine(HttpPost(url, dic));
     }
 }
--- Assets/Scripts/UtilComponent.cs
+++ Assets/Scripts/UtilComponent.cs
@@ -509,6 +509,45 @@ public class UtilComponent : MonoBehaviour {
 			callback (www.text);
 		}
 	}
+	protected IEnumerator HttpPost(string url, Dictionary<string, string> post)
+    {
+        WWWForm form = new WWWForm();
+        foreach (KeyValuePair<string, string> post_arg in post)
+        {
+        	//Debug.Log(post_arg.Key+", "+post_arg.Value);
+        	form.AddField(post_arg.Key, post_arg.Value);
+        }
+        WWW www = new WWW(url, form);
+
+        yield return StartCoroutine(CheckTimeOut(www, 10f));
+
+        if (www.error != null)
+        {
+            Debug.Log("HttpPost NG: " + www.error);
+        }
+        else if (www.isDone)
+        {
+            Debug.Log("HttpPost OK: " + www.text);
+        }
+    }
+
+    // HTTPリクエストのタイムアウト処理
+    IEnumerator CheckTimeOut(WWW www, float timeout)
+    {
+        float requestTime = Time.time;
+
+        while (!www.isDone)
+        {
+            if (Time.time - requestTime < timeout)
+                yield return null;
+            else
+            {
+                Debug.Log("TimeOut"); 
+                break;
+            }
+        }
+        yield return null;
+    }
 
 	#endregion
 }
