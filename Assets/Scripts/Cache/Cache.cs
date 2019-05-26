3b7daee68449e77cee17a7b7e44d8ff5a57d5d8b
Toshiki Ono <zuginosuke@gmail.com>
2019-05-26 13:04:18 +0900

    キャッシュクラス作成

---
+++ Assets/Scripts/Cache/Cache.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static partial class Cache
+{
+	public enum CacheGroup : int {
+		User = 0
+	}
+
+	private static Dictionary<CacheGroup, CacheBase> cacheHash;
+
+	public static User user;
+
+	public static void Prepare()
+	{
+		cacheHash = new Dictionary<CacheGroup, CacheBase> {
+			{ CacheGroup.User, user = new User() }
+		};
+	}
+
+	public static void Clear(CacheGroup cacheGroup)
+	{
+		switch (cacheGroup) {
+		case CacheGroup.User:
+			cacheHash[cacheGroup] = user = new User();
+			break;
+		}
+	}
+
+	public static void Unload()
+	{
+		foreach (CacheBase target in cacheHash.Values) {
+			target.Unload();
+		}
+	}
+}
---
+++ Assets/Scripts/Cache/CacheUser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static partial class Cache
+{
+	public class User : CacheBase
+	{
+		public override void Unload()
+		{
+		}
+	}
+}
