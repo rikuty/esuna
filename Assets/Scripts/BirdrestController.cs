6200f3d8a436ce7c4d6e77886d95f3415ed668cb
LunaSun <kawano@luna-sun.com>
2019-07-04 20:49:41 +0900

    小鳥モーション修正

---
+++ Assets/Scripts/BirdrestController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdrestController : MonoBehaviour {
+
+	[SerializeField] private List<LittleBirdAnimationController> birdList;
+	// Use this for initialization
+
+	private int currentBirdIndex = 0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) { 
+			LittleBirdAnimationController currentBird = birdList[currentBirdIndex];
+			currentBirdIndex++;
+
+			currentBird.gameObject.SetActive(true);
+		}
+	}
+}
--- Assets/Scripts/LittleBirdAnimationController.cs
+++ Assets/Scripts/LittleBirdAnimationController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class LittleBirdAnimationController : MonoBehaviour {
+
+	[SerializeField] private Animator littleBirdAnimator;
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +15,14 @@ public class LittleBirdAnimationController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void SetAction(){
+
+		// Z座標の初期化
+		this.transform.localPosition = new Vector3(this.transform.localPosition.x, 0, 0);
+
+		int actionNumber = UnityEngine.Random.Range(1, 6);
+		littleBirdAnimator.SetInteger("idleStatus", actionNumber);
 	}
 }
